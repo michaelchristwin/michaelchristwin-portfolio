341ab6fd02d1056980b22269c922cdb2c06e966c
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-06-28 15:56:29 +0100

    Peace

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,40 +1,33 @@
-import { useEffect, useRef, useState } from "react";
+import { useLayoutEffect, useRef } from "react";
 
 function Hero() {
   const firstLineRef = useRef(null);
   const secondLineRef = useRef(null);
   const thirdLineRef = useRef(null);
-  const [animationStarted, setAnimationStarted] = useState(false);
 
-  useEffect(() => {
-    const animateLines = () => {
+  useLayoutEffect(() => {
+    const animateLines = (ref: any, delay: any) => {
+      const node = ref.current;
       setTimeout(() => {
-        setAnimationStarted(true);
-      }, 100); // Delay the animation start by 100ms
+        node.classList.add("typing");
+      }, delay);
     };
 
-    animateLines();
+    animateLines(firstLineRef, 0);
+    animateLines(secondLineRef, 1700);
+    animateLines(thirdLineRef, 3200);
   }, []);
 
   return (
     <div className="hero lg:block flex">
       <div>
-        <span
-          ref={firstLineRef}
-          className={`block wel ${animationStarted ? "typing" : ""}`}
-        >
+        <span ref={firstLineRef} className="block wel">
           Welcome to my portfolio
         </span>
-        <span
-          ref={secondLineRef}
-          className={`block front ${animationStarted ? "typing" : ""}`}
-        >
+        <span ref={secondLineRef} className="block front">
           {`I'm a Frontend`}
         </span>
-        <span
-          ref={thirdLineRef}
-          className={`block front ${animationStarted ? "typing" : ""}`}
-        >
+        <span ref={thirdLineRef} className="block front">
           Developer
         </span>
       </div>
